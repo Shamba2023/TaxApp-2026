7b9dd886214bbe2e8760a867c380a7fb28a6e504
Shamba2023 <149244931+Shamba2023@users.noreply.github.com>
2026-05-10 21:30:20 +0530

    Refactor ReactDOM rendering with StrictMode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,10 @@
-import "./index.css"; // <--- This connects the Tailwind logic to your app
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// ... rest of the file
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import "./index.css"; 
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 );
